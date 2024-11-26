1f07a270870bb188dbb7ab6aa4167b10ada0ef32
Kirsty Balfour <kirstybalfour2003@yahoo.co.uk>
2024-11-26 16:36:30 +0000

    Closes #6 - Logout works on deployed and locally

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { Router, RouterLink } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
@@ -10,9 +10,17 @@ import { AuthenticationService } from '../services/authentication.service';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  currentUser!: any;
 
   constructor(private authService: AuthenticationService, private router: Router) {}
+
+  ngOnInit(): void {
+      this.authService.currentUser.subscribe(user => {
+        this.currentUser = user.user;
+        console.log(this.currentUser);
+      })
+  }
   
   logout() {
     this.authService.logout();
